6adacee3164220de7981be1a6f8efbc8b1fba930
Leo Fabiano Alves <leo.fabiano@gmail.com>
2022-09-03 15:22:39 -0300

    Externalizing configurations using environment variables

--- backend/src/main/java/io/leofalves/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/io/leofalves/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package io.leofalves.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,16 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -38,11 +49,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("dscatalog")                          // application name
-		.secret(passwordEncoder.encode("dscatalog123"))   // application secret
-		.scopes("read", "write")                          // access types
-		.authorizedGrantTypes("password")                 // password is the default for oauth2
-		.accessTokenValiditySeconds(86400);               // expiry time -> 24 hours = 86400
+		.withClient(clientId)                            // application name
+		.secret(passwordEncoder.encode(clientSecret))    // application secret
+		.scopes("read", "write")                         // access types
+		.authorizedGrantTypes("password")                // password is the default for oauth2
+		.accessTokenValiditySeconds(jwtDuration);        // expiry time -> 24 hours = 86400
 
 	}
 
